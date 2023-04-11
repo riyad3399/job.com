b2e493a26b47624e94bded9e89e7b355d87d3d24
riyad3399 <programminghero.riyad@gmail.com>
2023-04-11 17:55:30 +0600

    added the view details routs

--- src/components/JobDetails/JobDetails.jsx
+++ src/components/JobDetails/JobDetails.jsx
@@ -1,6 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { useLoaderData, useParams } from "react-router-dom";
-import { MapPinIcon, CurrencyDollarIcon } from "@heroicons/react/24/solid";
+import {
+  MapPinIcon,
+  CurrencyDollarIcon,
+  PhoneIcon,
+  EnvelopeIcon,
+  BriefcaseIcon,
+} from "@heroicons/react/24/solid";
 
 const JobDetails = () => {
   const items = useLoaderData();
@@ -26,27 +32,39 @@ const JobDetails = () => {
     worker,
   } = details;
   return (
-    <div className="container mx-8 flex gap-10 ">
-      <div className="space-y-6 w-3/5">
-        <p>Job Description: {job_description}</p>
-        <p>Job Responsibility: {job_responsibility}</p>
-        <p>Educational Requirements:{educational_requirements}</p>
-        <p>Experiences: {experiences}</p>
+    <div className="container mx-8 grid md:grid-cols-2 gap-8 mt-32">
+      <div className="space-y-6">
+        <p><span className="font-semibold text-lg text-gray-700">Job Description:</span> {job_description}</p>
+        <p><span className="font-semibold text-lg text-gray-700">Job Responsibility:</span> {job_responsibility}</p>
+        <p><span className="font-semibold text-lg text-gray-700">Educational Requirements:</span> {educational_requirements}</p>
+        <p> <span className="font-semibold text-lg text-gray-700">Experiences:</span> {experiences}</p>
       </div>
-      <div className="w-2/5 ml-10">
+      <div className=" ml-10  px-24">
         <h3 className="font-semibold text-xl mb-6">Job Details</h3>
-        <div className="flex gap-2">
+        <div className="flex gap-2 my-3">
           <CurrencyDollarIcon className="h-6 w-6 text-gray-400" />
           <p className="font-medium"> Salary: {salary}</p>
         </div>
         <div className="flex gap-2">
-          <MapPinIcon className="h-6 w-6 text-gray-400" />
+          <BriefcaseIcon className="h-6 w-6 text-gray-400" />
           <p className="font-medium">Job Title: {location}</p>
         </div>
+        <div className="my-6 space-y-3">
+          <h3 className="font-semibold text-xl mb-6">Contact Information</h3>
+          <div className="flex gap-2">
+            <PhoneIcon className="w-6 h-6 text-gray-400" />
+            <p>Phone: {phone}</p>
           </div>
-          <div>
-              <h3 className="font-semibold text-xl mb-6">Contact Information</h3>
+          <div className="flex gap-2">
+            <EnvelopeIcon className="w-6 h-6 text-gray-400" />
+            <p>Email: {email}</p>
           </div>
+          <div className="flex gap-2">
+            <MapPinIcon className="w-6 h-6 text-gray-400" />
+            <p>Address: {location}</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
